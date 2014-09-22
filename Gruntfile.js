9b7067e5a3eb2e9b93fec19ae32d994e952536c0
λhmad λjmi <ahmadajmi@outlook.com>
2014-09-22 11:10:16 +0300

    Added new helpers and refactoring old ones Added grunt to comile, minify and watch SASS Fixed border-radius variable

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt){
+
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		watch: {
+			css: {
+				files: ['css/**/*.scss'],
+				tasks: ['buildcss']
+			}
+		},
+		cssmin: {
+			build: {
+				src: 'helpers.css',
+				dest: 'helpers.min.css'
+			}
+		},
+		sass: {
+			build: {
+				files: {
+					'helpers.css': 'helpers.scss'
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['buildcss']);
+	grunt.registerTask('buildcss',  ['sass', 'cssmin']);
+
+};
